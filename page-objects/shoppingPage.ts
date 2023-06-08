7ffcded1570b53778eea36673aa394887881cf9a
Mahdi GHARSALLAH <Mahdi.GHARSALLAH@Generali.com>
2023-06-08 17:09:18 +0200

    updated screenshot statement

--- page-objects/shoppingPage.ts
+++ page-objects/shoppingPage.ts
@@ -1,41 +1,40 @@
 import { expect } from "@playwright/test";
 
 export class ShoppingPage {
-    private page: any;
+  private page: any;
 
-    constructor(page: any) {
-        this.page = page;
-    }
+  constructor(page: any) {
+    this.page = page;
+  }
 
-/*     async waitForPageLoad() {
+  /*     async waitForPageLoad() {
         await this.page.waitForSelector('[data-test="product_sort_container"]');
         await expect(this.page.getByText('Products').isVisible());
 
     } */
 
-    async checkPageDisplay() {
+  async checkPageDisplay() {
+    await this.page.waitForSelector('[data-test="product_sort_container"]');
+    //await expect(this.page.getByText('Products').isVisible());
+    expect.soft(await this.page.screenshot()).toMatchSnapshot("shopping.png");
+  }
 
-        await this.page.waitForSelector('[data-test="product_sort_container"]');
-        //await expect(this.page.getByText('Products').isVisible());
-        await expect(this.page).toHaveScreenshot("shopping.png");
-    }
+  async addToCart(itemName: string) {
+    await this.page.click(
+      `[data-test="add-to-cart-${itemName.toLowerCase().replace(/ /g, "-")}"]`
+    );
+  }
 
-    async addToCart(itemName: string) {
-        await this.page.click(`[data-test="add-to-cart-${itemName.toLowerCase().replace(/ /g, '-')}"]`);
-    }
+  async openCart() {
+    //await this.page.click('[data-test="shopping-cart-button"]');
+    await this.page.locator("a").filter({ hasText: "2" }).click();
+  }
 
-    async openCart() {
-        //await this.page.click('[data-test="shopping-cart-button"]');
-        await this.page.locator('a').filter({ hasText: '2' }).click();
-    }
+  async openMenu() {
+    await this.page.getByRole("button", { name: "Open Menu" }).click();
+  }
 
-    async openMenu() {
-
-        await this.page.getByRole('button', { name: 'Open Menu' }).click();
-
-    }
-
-    async logout() {
-        await this.page.getByRole('link', { name: 'Logout' }).click();
-    }
+  async logout() {
+    await this.page.getByRole("link", { name: "Logout" }).click();
+  }
 }
